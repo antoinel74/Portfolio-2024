217b13eca171215cb9adcb2bb22c59fffbb23d77
antoinel74 <antoine.lansman@gmail.com>
2023-12-12 14:25:07 +0100

    (navbar): rearrange menu position

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,18 +4,18 @@ export const Header = () => {
   return (
     <div className="relative min-h-screen flex flex-col items-center justify-center">
       <div>
-        <h1 className="absolute top-1/3 right-1/2 text-4xl md:text-6xl flex flex-col font-bold uppercase">
+        <h1 className="absolute top-1/3 left-[10%] text-4xl md:text-6xl flex flex-col font-bold uppercase">
           Antoine Lansman
           <span> Front-end</span>
           <span> Developer</span>
         </h1>
-        <div className="absolute flex flex-col gap-4 text-xl bottom-1/5 left-1/2 max-w-[400px]">
-          <img src="/svg/darkmodeToggle.svg" className="h-6 w-12" />
+        <div className="absolute flex flex-col gap-4 text-xl bottom-1/5 right-[10%] max-w-[400px]">
+          <img src="/svg/darkmodeToggle.svg" className="h-6 w-12 ml-12" />
           <p>
             Hello world ! I’m a junior front-end developer with a passion for
             clean and visually appealing design.
           </p>
-          <a className="text-sm bg-[#FE8A8A] text-black p-3 rounded-3xl w-[160px] text-center ml-auto">
+          <a className="text-sm bg-[#FE8A8A] text-black p-3 rounded-3xl w-[160px] text-center ml-auto hover:bg-[#FE6E6E] cursor-pointer">
             Explore my Works
           </a>
         </div>
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 export const Navbar = () => {
   return (
-    <nav className="fixed">
-      <ul className="w-full flex justify-center gap-4 py-4 font-light">
+    <nav className="fixed w-full">
+      <ul className="flex justify-end gap-4 py-4 font-light mx-6">
         <li>HOME</li>
         <li>WORKS</li>
         <li>CONTACT</li>
