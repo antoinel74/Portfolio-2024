032ba7a77713c666ae6503fd4500e09921a44896
antoinel74 <antoine.lansman@gmail.com>
2023-12-12 15:04:37 +0100

    (hero): layout done but needs cleaning

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,31 +3,34 @@ import SectionTitle from "./SectionTitle";
 
 export const Header = () => {
   return (
-    <div className="relative min-h-screen">
+    <div className="flex flex-wrap flex-col relative min-h-screen items-center justify-center">
       <SectionTitle
         title="Open to work !"
         busy={false}
         svg={false}
         className="absolute top-24 left-[5%]"
       />
-      <div>
-        <h1 className="absolute top-36 md:top-1/3 left-[10%] text-5xl md:text-6xl flex flex-col font-bold uppercase">
+      <div className="flex flex-col w-full md:w-[1000px] px-4">
+        <h1 className="text-4xl md:text-6xl flex flex-col font-bold uppercase text-left">
           Antoine Lansman
           <span> Front-end</span>
           <span> Developer</span>
         </h1>
-        <div className="absolute flex flex-col gap-4 text-xl bottom-1/4 md:bottom-1/5 right-[10%] max-w-[250px] md:max-w-[400px]">
-          <img
-            src="/svg/darkmodeToggle.svg"
-            className="hidden md:block h-6 w-12 ml-12"
-          />
-          <p>
-            Hello world ! I’m a junior front-end developer with a passion for
-            clean and visually appealing design.
-          </p>
-          <a className="text-sm bg-[#FE8A8A] text-black p-3 rounded-3xl w-[160px] text-center ml-auto hover:bg-[#FE6E6E] cursor-pointer">
-            Explore my Works
-          </a>
+        <div className="flex flex-wrap items-center gap-2 justify-end md:justify-end mt-6 md:mt-0 w-full">
+          <img src="/svg/arrow-bottom-left-large.svg" className="h-16 w-16" />
+          <div className="flex flex-col gap-4 text-sm md:text-xl max-w-[250px] md:max-w-[400px] text-right">
+            <img
+              src="/svg/darkmodeToggle.svg"
+              className="hidden md:block h-6 w-12 ml-24"
+            />
+            <p>
+              Hello world ! I’m a junior front-end developer with a passion for
+              clean and visually appealing design.
+            </p>
+            <a className="text-sm bg-[#FE8A8A] text-black p-3 rounded-3xl w-[160px] text-center ml-auto hover:bg-[#FE6E6E] cursor-pointer">
+              Explore my Works
+            </a>
+          </div>
         </div>
       </div>
     </div>
