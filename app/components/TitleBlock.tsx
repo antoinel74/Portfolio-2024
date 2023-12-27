f1d46ed87cc69e13b17f28318fbb7129b79d748a
antoinel74 <antoine.lansman@gmail.com>
2023-12-27 11:05:04 +0100

    replace img by Image component

--- app/components/TitleBlock.tsx
+++ app/components/TitleBlock.tsx
@@ -1,10 +1,11 @@
 import React, { forwardRef, Ref } from "react";
+import Image from "next/image";
 
 export const TitleBlock = forwardRef<HTMLHeadingElement>((_, ref: Ref<HTMLHeadingElement>) => {
   return (
     <h1 className="text-4xl md:text-5xl flex flex-col w-full md:w-1/2 gap-2" ref={ref} id="main-title">
       <span className="flex gap-2 items-center">
-        Creative mind <img src="/svg/and.svg" className="md:h-7 md:w-18 h-5 w-16" alt="and" />
+        Creative mind <Image src="/svg/and.svg" height={18} width={16} className="md:h-7 md:w-18 h-5 w-16" alt="and" />
       </span>
       <span>Front-end developer</span>
       <span>currently based in Belgium</span>
