79a4983eb6b69c431172c487d5205b6d21840f1c
antoinel74 <antoine.lansman@gmail.com>
2024-01-02 11:43:34 +0100

    navbar: fix hiding animation

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -35,7 +35,7 @@ export const Navbar = () => {
   }, []);
 
   return (
-    <nav className="fixed w-full z-10 nav">
+    <nav className="fixed w-full z-10 nav -translate-y-10">
       <ul className="flex justify-end text-sm gap-12 py-4 mx-6">
         <li>
           <Link href="/" className="relative links">
