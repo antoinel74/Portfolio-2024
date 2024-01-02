9b5ab83634ad3b72ab8467d85b854b3159de3500
antoinel74 <antoine.lansman@gmail.com>
2024-01-02 12:57:01 +0100

    style adjustement

--- app/components/DescriptionBlock.tsx
+++ app/components/DescriptionBlock.tsx
@@ -4,7 +4,7 @@ import { BusyStatus } from "./BusyStatus";
 export const DescriptionBlock = forwardRef<HTMLHeadingElement>((_, ref: Ref<HTMLHeadingElement>) => {
   return (
     <div className="flex flex-col gap-6 w-full md:w-1/2 md:pl-12 opacity-0 y-0" ref={ref}>
-      <p className="w-2/3">
+      <p className="w-2/3 max-w-[500px]">
         Hey ! I&apos;m Antoine Lansman, a junior front end developer with a passion for clean and visually appealing
         design. I write code and have experience in web communication.
       </p>
