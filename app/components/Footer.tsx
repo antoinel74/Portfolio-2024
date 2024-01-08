9c14f4f38fe73c13d93f7b6f28b123746ba3dbfd
antoinel74 <antoine.lansman@gmail.com>
2024-01-08 10:24:46 +0100

    update date

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <footer className="w-full py-2 px-2 md:px-6 flex flex-col gap-2 items-end dark:text-white opacity-70 font-thin">
       <span className="w-full border-t border-gray-200"></span>
-      <span>antoine lansman &#174; 2023</span>
+      <span>antoine lansman &#174; 2024</span>
     </footer>
   );
 };
