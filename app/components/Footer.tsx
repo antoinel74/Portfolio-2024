4b9fba1212be1a73f92dd4dcd036cfc8940ddb2e
antoinel74 <antoine.lansman@gmail.com>
2024-02-12 15:59:20 +0100

    fix colors

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <footer className="font-ibm w-full py-8 pl-4 opacity-60 dark:text-white">
+    <footer className="font-ibm border-t border-white w-full py-8 pl-4 opacity-60 dark:text-white">
       <span className="block md:w-1/2 w-full md:text-center">ANTOINE LANSMAN &#47; &#169; 2024</span>
     </footer>
   );
