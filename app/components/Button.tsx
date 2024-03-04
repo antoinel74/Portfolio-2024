9e40c9a1f65386554b7e5e11977d1f057b4eb922
antoinel74 <antoine.lansman@gmail.com>
2024-03-04 12:03:12 +0100

    refactor btn

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -3,6 +3,7 @@ import { PrismicNextLink } from "@prismicio/next";
 
 export const Button = (props: { linkText: string; link: string }) => {
   const { linkText, link } = props;
+
   return (
     <PrismicNextLink href={link} className="button px-4 md:px-0 uppercase links invert dark:invert-0">
       {linkText}
