3cca43ff0f20f932c87dd81e7f3c025f07da2752
antoinel74 <antoine.lansman@gmail.com>
2024-03-04 13:29:26 +0100

    refactor anim

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -17,11 +17,11 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ year, title, img, uid }) => {
     // Reveal animation
     gsap.fromTo(
       el,
-      { scale: 0.7, opacity: 0 },
+      { scale: 0.6, opacity: 0 },
       {
         scale: 1,
         opacity: 1,
-        duration: 0.5,
+        duration: 0.6,
         stagger: 1,
         scrollTrigger: {
           trigger: el,
@@ -31,15 +31,14 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ year, title, img, uid }) => {
   }, []);
 
   return (
-    <div className="w-full overflow-hidden relative group">
+    <div className="w-full overflow-hidden relative group opacity-0" ref={ref}>
       <div className="pointer-events-none opacity-0 text-lg z-50 bg-white rounded-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-black font-light w-32 h-32 flex justify-center items-center group-hover:opacity-100 scale-50 group-hover:scale-110 duration-300">
         <span>See Project</span>
       </div>
       {img && (
         <PrismicNextLink href={`/projects/${uid}`}>
           <figure
-            className={`relative w-full h-full min-h-[50vh] rounded-md overflow-hidden font-ibm uppercase text-xs opacity-0`}
-            ref={ref}
+            className={`relative w-full h-full min-h-[50vh] rounded-md overflow-hidden font-ibm uppercase text-xs`}
           >
             <PrismicNextImage field={img} fill className="absolute rounded-md w-full h-full object-cover" />
             <figcaption className="absolute top-2 left-2 bg-black rounded-xl px-2 opacity-50 text-white">
